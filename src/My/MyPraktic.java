3094ce62aee44e12408469312dad0a683628da5e
Vetal <stepko2006@ukr.net>
2019-12-16 17:27:04 +0200

    add a new fitch

--- src/My/MyPraktic.java
+++ src/My/MyPraktic.java
@@ -517,3 +517,17 @@ class Overloading {
         new Tree();
     }
 }
+//Сумма введених двох чисел
+class MyFirstClass
+{
+    public static void main(String[] args)
+    {
+        int a, b, c;
+        String s1, s2;
+        System.out.println("Введите два числа");
+        a = new Scanner(System.in).nextInt();
+        b = new Scanner(System.in).nextInt();
+        c = a + b;
+        System.out.println("Сумма ваших чисел равна " + c);
+    }
+}
